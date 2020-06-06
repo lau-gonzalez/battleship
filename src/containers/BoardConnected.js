08c3324c27bed36048fd61a417aaf7ba37912ec2
lau-gonzalez <laau.gonzaalez@hotmail.com>
2020-06-06 19:00:17 -0300

    Show boards and create initial state

--- src/containers/BoardConnected.js
+++ src/containers/BoardConnected.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import Board from '../components/Board';
+import Board from './../components/Board';
 import { preGameBoard } from '../actions/index';
 import { createSelector } from 'reselect';
 
